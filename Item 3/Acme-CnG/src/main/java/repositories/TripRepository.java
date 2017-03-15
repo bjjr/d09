bbe4c98a5b7e7c4fff5216934d38738db550b188
javiparra17 <javierparraoyola619@hotmail.com>
2017-03-15 12:37:02 +0100

    Trip repository and service

--- Item 3/Acme-CnG/src/main/java/repositories/TripRepository.java
+++ Item 3/Acme-CnG/src/main/java/repositories/TripRepository.java
@@ -12,7 +12,16 @@ import domain.Trip;
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Integer> {
 
+	@Query("select count(o)/(select count(r) from Request r) from Offer o")
+	Double findRatioOfferVersusRequest();
+
 	@Query("select t from Trip t where t.customer.id = ?1")
 	Collection<Trip> findTripsByCustomer(int customerId);
+
+	@Query("select t from Trip t where t.title like %?1% or t.description like %?1% or t.origin like %?1% or t.destination like %?1%")
+	Collection<Trip> findByKeyword(String keyword);
+
+	@Query("select t from Trip t where t.banned=FALSE")
+	Collection<Trip> findAllNotBanned();
 
 }
