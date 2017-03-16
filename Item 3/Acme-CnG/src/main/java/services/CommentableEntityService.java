fc4b8fcbc33524a87ad87c625e778760e673b174
Javier Santos <me@javiersantos.me>
2017-03-17 00:27:40 +0100

    Update CommentableEntity service

--- Item 3/Acme-CnG/src/main/java/services/CommentableEntityService.java
+++ Item 3/Acme-CnG/src/main/java/services/CommentableEntityService.java
@@ -4,6 +4,7 @@ package services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.CommentableEntityRepository;
 import domain.CommentableEntity;
@@ -12,12 +13,30 @@ import domain.CommentableEntity;
 @Service
 public class CommentableEntityService {
 
+	//Managed repository
+
 	@Autowired
 	private CommentableEntityRepository	commentableEntityRepository;
 
 
 	public CommentableEntity findOne(final int id) {
-		return this.commentableEntityRepository.findOne(id);
+		final CommentableEntity res = this.commentableEntityRepository.findOne(id);
+		Assert.notNull(res);
+		return res;
+	}
+
+	public CommentableEntity save(final CommentableEntity ce) {
+		CommentableEntity res;
+		Assert.notNull(ce);
+
+		res = this.commentableEntityRepository.save(ce);
+		Assert.notNull(res);
+
+		return res;
+	}
+
+	public void flush() {
+		this.commentableEntityRepository.flush();
 	}
 
 }
