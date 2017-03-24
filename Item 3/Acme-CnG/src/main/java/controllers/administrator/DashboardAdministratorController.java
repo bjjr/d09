f935df7ea789d0ecd09d1b8f5f60da6f89afab95
bermundom <berny.sevillista@gmail.com>
2017-03-24 17:25:34 +0100

    Dashboard controller finished

--- Item 3/Acme-CnG/src/main/java/controllers/administrator/DashboardAdministratorController.java
+++ Item 3/Acme-CnG/src/main/java/controllers/administrator/DashboardAdministratorController.java
@@ -1,222 +1,188 @@
-//
-// package controllers.administrator;
-//
-// import java.util.ArrayList;
-// import java.util.Collection;
-//
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.servlet.ModelAndView;
-//
-// import services.ApplicationService;
-// import services.CommentService;
-// import services.CustomerService;
-// import services.MessageService;
-// import services.OfferService;
-// import services.RequestService;
-// import services.TripService;
-// import controllers.AbstractController;
-//
-// @Controller
-// @RequestMapping("/dashboard/administrator")
-// public class DashboardAdministratorController extends AbstractController {
-//
-// // Supporting services -----------------------------------------------------------
-//
-// @Autowired
-// private ApplicationService applicationService;
-//
-// @Autowired
-// private TripService tripService;
-//
-// @Autowired
-// private OfferService offerService;
-//
-// @Autowired
-// private RequestService requestService;
-//
-// @Autowired
-// private MessageService messageService;
-//
-// @Autowired
-// private CustomerService customerService;
-//
-// @Autowired
-// private CommentService commentService;
-//
-//
-// // Constructors -----------------------------------------------------------
-//
-// public DashboardAdministratorController() {
-// super();
-// }
-//
-// // Dashboard -----------------------------------------------------------
-//
-// @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-// public ModelAndView dashboard() {
-//
-// ModelAndView result;
-// Double bookAPL;
-// Double bookDPL;
-// Double bookAPT;
-// Double bookDPT;
-// Collection<Lessor> lessorMBA;
-// Collection<Lessor> lessorMBD;
-// Collection<Lessor> lessorMBP;
-// Collection<Tenant> tenantMBA;
-// Collection<Tenant> tenantMBD;
-// Collection<Tenant> tenantMBP;
-// Collection<Lessor> lessorMaxABB;
-// Collection<Lessor> lessorMinABB;
-// Collection<Tenant> tenantMaxABB;
-// Collection<Tenant> tenantMinABB;
-// Lessor maxL;
-// Lessor minL;
-// Tenant maxT;
-// Tenant minT;
-// Double maxRPF;
-// Double minRPF;
-// Double avgRPF;
-// Double maxAPP;
-// Double minAPP;
-// Double avgAPP;
-// Collection<Attribute> attributeSMTUP;
-// Double maxSIPA;
-// Double minSIPA;
-// Double avgSIPA;
-// Double maxIPT;
-// Double minIPT;
-// Double avgIPT;
-// Double totalMoney;
-// Double avgBPP1A;
-// Double avgBPPNA;
-//
-// bookAPL = this.numbers(this.lessorService.avgAcceptedPerLessor());
-// bookDPL = this.numbers(this.lessorService.avgDeniedPerLessor());
-// bookAPT = this.numbers(this.tenantService.avgAcceptedPerTenant());
-// bookDPT = this.numbers(this.tenantService.avgDeniedPerTenant());
-// lessorMBA = this.lessors(this.lessorService.lessorNumApproved());
-// lessorMBD = this.lessors(this.lessorService.lessorNumDenied());
-// lessorMBP = this.lessors(this.lessorService.lessorNumPending());
-// tenantMBA = this.tenants(this.tenantService.tenantsMoreRequestsApproved());
-// tenantMBD = this.tenants(this.tenantService.tenantsMoreRequestsDenied());
-// tenantMBP = this.tenants(this.tenantService.tenantsMoreRequestsPending());
-// lessorMaxABB = new ArrayList<Lessor>();
-// lessorMinABB = new ArrayList<Lessor>();
-// maxL = this.lessorService.lessorMaxRatio();
-// minL = this.lessorService.lessorMinRatio();
-// if (maxL != null)
-// lessorMaxABB.add(maxL);
-// if (minL != null)
-// lessorMinABB.add(minL);
-// tenantMaxABB = new ArrayList<Tenant>();
-// tenantMinABB = new ArrayList<Tenant>();
-// maxT = this.tenantService.tenantMaxRatio();
-// minT = this.tenantService.tenantMinRatio();
-// if (maxT != null)
-// tenantMaxABB.add(maxT);
-// if (minT != null)
-// tenantMinABB.add(minT);
-// maxRPF = this.numbers(this.finderService.maxResultsPerFinder());
-// minRPF = this.numbers(this.finderService.minResultsPerFinder());
-// avgRPF = this.numbers(this.finderService.avgResultsPerFinder());
-// maxAPP = this.numbers(this.auditService.findMaxAuditsOfProperties());
-// minAPP = this.numbers(this.auditService.findMinAuditsOfProperties());
-// avgAPP = this.numbers(this.auditService.findAvgAuditsOfProperties());
-// attributeSMTUP = this.attributes(this.attributeService.findListAttributesSortedByTimesUsed());
-// maxSIPA = this.numbers(this.socialIdentityService.findMaxSocialIdentities());
-// minSIPA = this.numbers(this.socialIdentityService.findMinSocialIdentities());
-// avgSIPA = this.numbers(this.socialIdentityService.findAvgSocialIdentities());
-// maxIPT = this.numbers(this.invoiceService.findMaxInvoicesOfTenants());
-// minIPT = this.numbers(this.invoiceService.findMinInvoicesOfTenants());
-// avgIPT = this.numbers(this.invoiceService.findAvgInvoicesOfTenants());
-// totalMoney = this.numbers(this.invoiceService.totalMoney());
-// avgBPP1A = this.numbers(this.bookService.findAvgBooksProperty1Audit());
-// avgBPPNA = this.numbers(this.bookService.findAvgBooksPropertyNoAudit());
-//
-// result = new ModelAndView("administrator/dashboard");
-// result.addObject("bookAPL", bookAPL);
-// result.addObject("bookDPL", bookDPL);
-// result.addObject("bookAPT", bookAPT);
-// result.addObject("bookDPT", bookDPT);
-// result.addObject("lessorMBA", lessorMBA);
-// result.addObject("lessorMBD", lessorMBD);
-// result.addObject("lessorMBP", lessorMBP);
-// result.addObject("tenantMBA", tenantMBA);
-// result.addObject("tenantMBD", tenantMBD);
-// result.addObject("tenantMBP", tenantMBP);
-// result.addObject("lessorMaxABB", lessorMaxABB);
-// result.addObject("lessorMinABB", lessorMinABB);
-// result.addObject("tenantMaxABB", tenantMaxABB);
-// result.addObject("tenantMinABB", tenantMinABB);
-// result.addObject("maxRPF", maxRPF);
-// result.addObject("minRPF", minRPF);
-// result.addObject("avgRPF", avgRPF);
-// result.addObject("maxAPP", maxAPP);
-// result.addObject("minAPP", minAPP);
-// result.addObject("avgAPP", avgAPP);
-// result.addObject("attributeSMTUP", attributeSMTUP);
-// result.addObject("maxSIPA", maxSIPA);
-// result.addObject("minSIPA", minSIPA);
-// result.addObject("avgSIPA", avgSIPA);
-// result.addObject("maxIPT", maxIPT);
-// result.addObject("minIPT", minIPT);
-// result.addObject("avgIPT", avgIPT);
-// result.addObject("totalMoney", totalMoney);
-// result.addObject("avgBPP1A", avgBPP1A);
-// result.addObject("avgBPPNA", avgBPPNA);
-// result.addObject("requestURI", "dashboard/administrator/dashboard.do");
-//
-// return result;
-//
-// }
-// //Ancillary methods -----------------------------------
-//
-// public Collection<Lessor> lessors(final Collection<Lessor> lessors) {
-// Collection<Lessor> result;
-//
-// result = new ArrayList<Lessor>();
-//
-// if (lessors != null)
-// result.addAll(lessors);
-//
-// return result;
-// }
-//
-// public Collection<Tenant> tenants(final Collection<Tenant> tenants) {
-// Collection<Tenant> result;
-//
-// result = new ArrayList<Tenant>();
-//
-// if (tenants != null)
-// result.addAll(tenants);
-//
-// return result;
-// }
-//
-// public Collection<Attribute> attributes(final Collection<Attribute> attributes) {
-// Collection<Attributee> result;
-//
-// result = new ArrayList<Attribute>();
-//
-// if (attributes != null)
-// result.addAll(attributes);
-//
-// return result;
-// }
-//
-// public Double numbers(final Double number) {
-// Double result;
-//
-// result = 0.0;
-//
-// if (number != null)
-// result = number;
-//
-// return result;
-// }
-//
-//}
+
+package controllers.administrator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.ActorService;
+import services.ApplicationService;
+import services.CommentService;
+import services.CustomerService;
+import services.MessageService;
+import services.OfferService;
+import services.RequestService;
+import services.TripService;
+import controllers.AbstractController;
+import domain.Actor;
+import domain.Customer;
+
+@Controller
+@RequestMapping("/dashboard/administrator")
+public class DashboardAdministratorController extends AbstractController {
+
+	// Supporting services -----------------------------------------------------------
+
+	@Autowired
+	private ActorService		actorService;
+
+	@Autowired
+	private ApplicationService	applicationService;
+
+	@Autowired
+	private TripService			tripService;
+
+	@Autowired
+	private OfferService		offerService;
+
+	@Autowired
+	private RequestService		requestService;
+
+	@Autowired
+	private MessageService		messageService;
+
+	@Autowired
+	private CustomerService		customerService;
+
+	@Autowired
+	private CommentService		commentService;
+
+
+	// Constructors -----------------------------------------------------------
+
+	public DashboardAdministratorController() {
+		super();
+	}
+
+	// Dashboard -----------------------------------------------------------
+
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public ModelAndView dashboard() {
+
+		ModelAndView result;
+		final Double ratioOR;
+		final Double avgOC;
+		final Double avgRC;
+		final Double avgAO;
+		final Double avgAR;
+		final Collection<Customer> customerMAA;
+		final Collection<Customer> customerMAD;
+		final Double avgACA;
+		final Double avgACO;
+		final Double avgACR;
+		final Double avgACPA;
+		final Double avgACPC;
+		final Collection<Actor> actor10moreavgCPA;
+		final Collection<Actor> actor10lessavgCPA;
+		final Long maxMSA;
+		final Long minMSA;
+		final Double avgMSA;
+		final Long maxMRA;
+		final Long minMRA;
+		final Double avgMRA;
+		final Collection<Actor> actorsMMS;
+		final Collection<Actor> actorsMMR;
+
+		ratioOR = this.numbers(this.tripService.findRatioOfferVersusRequest());
+		avgOC = this.numbers(this.offerService.findAvgOfferPerCostumer());
+		avgRC = this.numbers(this.requestService.findAvgRequestPerCustomer());
+		avgAO = this.numbers(this.applicationService.findAvgApplicationsPerOffer());
+		avgAR = this.numbers(this.applicationService.findAvgApplicationsPerRequest());
+		customerMAA = this.customer(this.customerService.findCustomerWhoHasMoreApplicationsAccepted());
+		customerMAD = this.customer(this.customerService.findCustomerWhoHasMoreApplicationsDenied());
+		avgACA = this.numbers(this.commentService.findAverageCommentPerActor());
+		avgACO = this.numbers(this.commentService.findAverageCommentPerOffer());
+		avgACR = this.numbers(this.commentService.findAverageCommentPerRequest());
+		avgACPA = this.numbers(this.commentService.findAverageCommentPerAdministrator());
+		avgACPC = this.numbers(this.commentService.findAverageCommentPerCustomer());
+		actor10moreavgCPA = this.actors(this.actorService.findActorPostMore10PerCentAVGCommentsPerActor());
+		actor10lessavgCPA = this.actors(this.actorService.findActorPostLess10PerCentAVGCommentsPerActor());
+		maxMSA = this.messageService.findMaxNumSntMsgPerActor();
+		minMSA = this.messageService.findMinNumSntMsgPerActor();
+		avgMSA = this.numbers(this.messageService.findAvgNumSntMsgPerActor());
+		maxMRA = this.messageService.findMaxNumRecMsgPerActor();
+		minMRA = this.messageService.findMinNumRecMsgPerActor();
+		avgMRA = this.numbers(this.messageService.findAvgNumRecMsgPerActor());
+		actorsMMS = this.actor(this.actorService.findActorWithMoreSentMessages());
+		actorsMMR = this.actor(this.actorService.findActorWithMoreReceivedMessages());
+
+		result = new ModelAndView("administrator/dashboard");
+		result.addObject("requestURI", "dashboard/administrator/dashboard.do");
+		result.addObject("ratioOR", ratioOR);
+		result.addObject("avgOC", avgOC);
+		result.addObject("avgRC", avgRC);
+		result.addObject("avgAO", avgAO);
+		result.addObject("avgAR", avgAR);
+		result.addObject("customerMAA", customerMAA);
+		result.addObject("customerMAD", customerMAD);
+		result.addObject("avgACA", avgACA);
+		result.addObject("avgACO", avgACO);
+		result.addObject("avgACR", avgACR);
+		result.addObject("avgACPA", avgACPA);
+		result.addObject("avgACPC", avgACPC);
+		result.addObject("actor10moreavgCPA", actor10moreavgCPA);
+		result.addObject("actor10lessavgCPA", actor10lessavgCPA);
+		result.addObject("maxMSA", maxMSA);
+		result.addObject("minMSA", minMSA);
+		result.addObject("avgMSA", avgMSA);
+		result.addObject("maxMRA", maxMRA);
+		result.addObject("minMRA", minMRA);
+		result.addObject("avgMRA", avgMRA);
+		result.addObject("actorsMMS", actorsMMS);
+		result.addObject("actorsMMR", actorsMMR);
+
+		return result;
+
+	}
+	//Ancillary methods -----------------------------------
+
+	public Collection<Customer> customer(final Customer customer) {
+		Collection<Customer> result;
+
+		result = new ArrayList<Customer>();
+
+		if (customer != null)
+			result.add(customer);
+
+		return result;
+	}
+
+	public Collection<Actor> actor(final Actor actor) {
+		Collection<Actor> result;
+
+		result = new ArrayList<Actor>();
+
+		if (actor != null)
+			result.add(actor);
+
+		return result;
+	}
+
+	public Collection<Actor> actors(final Collection<Actor> actors) {
+		Collection<Actor> result;
+
+		result = new ArrayList<Actor>();
+
+		if (actors != null)
+			result.addAll(actors);
+
+		return result;
+	}
+
+	public Double numbers(final Double number) {
+		Double result;
+
+		result = 0.;
+
+		if (number != null)
+			result = number;
+
+		return result;
+	}
+
+}
